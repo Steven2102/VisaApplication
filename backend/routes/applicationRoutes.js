3d587726a1273bee5d06f0933807317ed0a8a285
Steven Baist <baist.steven@gmail.com>
2025-08-15 11:40:15 +1000

    EVPP-26 Sub Task 2.1: Create a component to view all applications.

--- backend/routes/applicationRoutes.js
+++ backend/routes/applicationRoutes.js
@@ -4,7 +4,7 @@ const { protect } = require('../middleware/authMiddleware');
 const router = express.Router();
 
 router.get('/', protect, getApplications);
-router.get('/:id', protect, getApplication);
+router.get('/:id', protect, getApplication); // Fetch a single application
 router.post('/', protect, addapplication); // Create new application
 router.put('/:id', protect, updateapplication); // Update application
 router.delete('/:id', protect, deleteapplication); // Delete application
