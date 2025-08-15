2f67458716dec9c9ccc8f29a74a04c4f8ef8a8c6
Steven Baist <baist.steven@gmail.com>
2025-08-15 16:51:09 +1000

    axios changed

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5001', // local
-  //baseURL: 'http://52.62.107.232:5001', // live
+  //baseURL: 'http://localhost:5001', // local
+  baseURL: 'http://3.27.235.121:5001', // live
   headers: { 'Content-Type': 'application/json' },
 });
 
