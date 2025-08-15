d3cb7e3e03206371f39047f069aa1e07842a8642
Steven Baist <baist.steven@gmail.com>
2025-08-15 12:25:54 +1000

    EVPP-65 Sub Task 1.4: Update the invoice list to display newly created invoices.

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   baseURL: 'http://localhost:5001', // local
-  //baseURL: 'http://52.62.107.232:5001', // live
+  //baseURL: 'http://52.62.107.232:5001', // live change for webserver
   headers: { 'Content-Type': 'application/json' },
 });
 
