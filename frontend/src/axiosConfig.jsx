47e7afff69062b1a01c091b3ef60440e41a00621
Steven Baist <baist.steven@gmail.com>
2025-08-15 12:31:33 +1000

    EVPP-69 Sub Task 3.2: Fetch details of the invoice from the database using the backend API.

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   baseURL: 'http://localhost:5001', // local comment out for later for AWS
-  //baseURL: 'http://52.62.107.232:5001', // live change for webserver
+  //baseURL: 'http://52.62.107.232:5001', // live, change for webserver
   headers: { 'Content-Type': 'application/json' },
 });
 
